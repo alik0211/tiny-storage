bc120a264ee4eaef39920058e0fc635c2e668e3a
Ali Gasymov <alik0211alik@gmail.com>
2018-08-19 10:11:56 +0400

    Add storage change

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-export default {
-  key(key) {
-    return localStorage.key(key);
-  },
-  get(name) {
-    return JSON.parse(localStorage.getItem(name));
-  },
-  set(name, value) {
-    return localStorage.setItem(name, JSON.stringify(value));
-  },
-  remove(name) {
-    return localStorage.removeItem(name);
-  },
-  clear() {
-    return localStorage.clear();
-  },
-  get length() {
-    return localStorage.length;
+class TinyStorage {
+  constructor(storage) {
+    this.storage = storage;
   }
-};
+  setStorage(storage) {
+    this.storage = storage;
+  }
+  key(key) {
+    return this.storage.key(key);
+  }
+  get(name) {
+    return JSON.parse(this.storage.getItem(name));
+  }
+  set(name, value) {
+    return this.storage.setItem(name, JSON.stringify(value));
+  }
+  remove(name) {
+    return this.storage.removeItem(name);
+  }
+  clear() {
+    return this.storage.clear();
+  }
+  get length() {
+    return this.storage.length;
+  }
+}
+
+const tinyStorage = new TinyStorage(localStorage);
+
+export default tinyStorage;
