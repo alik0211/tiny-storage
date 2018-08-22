f024fa8f4d61a6a80953f0e0a46d894e99892478
Ali Gasymov <alik0211alik@gmail.com>
2018-08-22 18:50:48 +0400

    Add the build for ES module

--- rollup.config.js
+++ rollup.config.js
@@ -19,9 +19,15 @@ export default [
   },
   {
     input: 'src/main.js',
-    output: {
-      file: pkg.main,
-      format: 'cjs'
-    }
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'esm'
+      }
+    ]
   }
 ];
